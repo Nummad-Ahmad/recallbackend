ca916f0c5db6412a35c5f0a6d9f472d57f558727
nummad <nummad222@gmail.com>
2025-01-24 11:36:01 +0500

    comit

--- backend/server.js
+++ backend/server.js
@@ -2,29 +2,70 @@ const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
-const mongoURI = 'mongodb://localhost:27017/'; 
-// const mongoURI = 'mongodb+srv://nummad:12345@cluster0.ceymr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+const userModel = require('./models/users');
+// const mongoURI = 'mongodb://localhost:27017/users';
+const bcrypt = require('bcrypt');
+const mongoURI = 'mongodb+srv://nummad:12345@cluster0.ceymr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
 
 dotenv.config();
 const app = express();
 const port = 3000;
 
 const corsOptions = {
-    // origin: process.env.frontendUrl || 'https://recallchatbot.vercel.app',
+    origin: '*',
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
 };
 
 app.use(cors(corsOptions));
+app.use(express.json());
 
-app.get('/', (req, res)=>{
+app.get('/', (req, res) => {
     res.send('Backend deployed successfully');
 })
 
+app.post('/login', async (req, res) => {
+    const { email, password } = req.body;
+    try {
+        const existingUser = await userModel.findOne({ email });
+
+        if (!existingUser) {
+            return res.status(400).json({ error: 'User not found. Please sign up first.' });
+        }
+        const isPasswordValid = await bcrypt.compare(password, existingUser.password);
+        if (!isPasswordValid) {
+            return res.status(401).json({ error: 'Invalid email or password' });
+        }
+        res.status(200).json({ message: 'Login successful', user: existingUser });
+    } catch (error) {
+        console.error('Error:', error);
+        res.status(500).json({ error: 'An error occurred while processing your request' });
+    }
+});
+
+app.post('/signup', async (req, res) => {
+    const { email, password } = req.body;
+    try {
+        const existingUser = await userModel.findOne({ email });
+        if (existingUser) {
+            return res.status(400).json({ error: 'Email already exists' });
+        }
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const newUser = await userModel.create({ email, password: hashedPassword });
+        res.status(201).json(newUser);
+    } catch (error) {
+        console.error('Error:', error);
+        res.status(500).json({ error: 'An error occurred while processing your request' });
+    }
+});
+
+
+
 
 mongoose.connect(mongoURI)
-  .then(() => console.log('Connected to MongoDB'))
-  .catch(err => console.error('Could not connect to MongoDB', err));
-app.listen(port, ()=>{
+    .then(() => console.log('Connected to MongoDB'))
+    .catch(err => console.log('Could not connect to MongoDB', err));
+
+app.listen(port, () => {
     console.log('server started');
 })
